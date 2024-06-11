077f4cd84e57b982f6835699ff4f801422717566
69ggg69 <103955916+69ggg69@users.noreply.github.com>
2024-06-11 18:18:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
 import React, { useState } from 'react';
-import './App.css';
+impost ./App.css;
 
-const App = () => {
-  const monthNames = [
-    'January', 'February', 'March', 'April', 'May', 'June', 
-    'July', 'August', 'September', 'October', 'November', 'December'
-  ];
-  const initialMonths = Array.from({ length: monthNames.length }, () => 1);
-  const [months, setMonths] = useState(initialMonths);
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
-  const incrementMonth = (index) => {
-    const newMonths = [...months];
-    newMonths[index] += 1;
-    setMonths(newMonths);
-  };
-
-  const decrementMonth = (index) => {
-    const newMonths = [...months];
-    if (newMonths[index] > 1) {
-      newMonths[index] -= 1;
-      setMonths(newMonths);
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
     }
   };
 
+  const handleDeleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="app">
-      {months.map((count, index) => (
-        <div key={index} className="month-container">
-          <div className="month">
-            <span>{monthNames[index]}</span>
-            <button onClick={() => incrementMonth(index)}>+</button>
-            <button onClick={() => decrementMonth(index)}>-</button>
-          </div>
-          {Array.from({ length: count }, (_, i) => (
-            <div key={i} className="month-content">
-              {i + 1}
-            </div>
-          ))}
-        </div>
-      ))}
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-};
+}
 
-export default App;
+export default TodoList;
